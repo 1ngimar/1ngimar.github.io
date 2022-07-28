385ca40857304ac0f7ca15e2b3ff3ae98972aa62
1ngimar <54596529+1ngimar@users.noreply.github.com>
2022-07-28 22:50:47 +0000

    advania-svg-animation-initial

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ window.onload = function() {
     // } catch (error) {
     //     console.log(error);
     // }
-    var body = document.getElementById('body');
-    var videoplayer = document.getElementById('videoplayer');
-    if (i == 0) {
-        body.addEventListener('mouseenter', e => videoplayer.play());
-    } else {
-        body.removeEventListener('mouseenter');
+    // var body = document.getElementById('body');
+    // var videoplayer = document.getElementById('videoplayer');
+    // if (i == 0) {
+    //     body.addEventListener('mouseenter', e => videoplayer.play());
+    // } else {
+    //     body.removeEventListener('mouseenter');
+    // }
+    const svg = document.getElementById('advania-logo-1');
+    svg.onclick = (e) => {
+        const colors = ['red', 'green', 'blue', 'orange', 'pink', 'purple'];
+        const rando = () => colors[Math.floor(Math.random() * colors.length)];
+        document.documentElement.style.cssText = `
+            --dark-color: ${rando()};
+            --light-color: ${rando()};
+        `
     }
 }
 
